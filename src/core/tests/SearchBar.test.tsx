9ed87457c3cf21f1426667636864a9a42f7c349f
iagomeijon <iagomeijonm@gmail.com>
2021-07-11 16:46:43 -0300

    create jest test

--- src/core/tests/SearchBar.test.tsx
+++ src/core/tests/SearchBar.test.tsx
@@ -4,7 +4,7 @@ import '@testing-library/jest-dom/extend-expect';
 import SearchBar from '../../components/SearchBar';
 
 
-describe("SearchBar teste", () => {
+describe("SearchBar button label test", () => {
     render(<SearchBar getSearch={() => {}} />);
     const {getByText} = screen;
     expect(getByText('Buscar')).toBeInTheDocument();
